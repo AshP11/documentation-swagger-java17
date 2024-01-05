41516c6819570b017a3ed4b231bbb981ba785b87
AshrafP1 <AshrafP1@verifone.com>
2024-01-06 02:00:24 +0530

    Initial commit

---
+++ src/main/java/com/example/documentationswaggerjava1/DocumentationSwaggerJava81Application.java
@@ -0,0 +1,13 @@
+package com.example.documentationswaggerjava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationSwaggerJava81Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentationSwaggerJava81Application.class, args);
+    }
+
+}
